a4c4beeaba650128fc1aea3c7050ec5f0011bfea
Teruteru <teruterubouzu1024@gmail.com>
2018-03-03 03:23:58 +0900

    Update for enumerating arguments and argument pointers

--- src/main.c
+++ src/main.c
@@ -3,8 +3,12 @@
 
 int main(int argc, char** argv){
     printf("test\n");
+    int c;
     if(argc > 1){
-        printf("test(%d, %p, %s)\n", argc, argv, argv[0]);
+        printf("test(%d, %p, %p, %s)\n", argc, argv, argv[0], argv[0]);
+        for(c = 1; c < argc; c++){
+            printf("%p: %s\n", argv[c], argv[c]);
+        }
     } else {
         printf("argc is %d.\n", argc);
     }
