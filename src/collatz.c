3937d2f4cddd0223351ad7d7f7b9df2f2507a330
Teruteru <teruterubouzu1024@gmail.com>
2018-03-03 04:15:10 +0900

    Update collatz function
    Optimize recursive function

--- src/collatz.c
+++ src/collatz.c
@@ -1,6 +1,6 @@
 
 static int collatz_rec(int n, int sub){
-    return  n > 1 ? collatz_rec((n & 1) ? n * 3 + 1 : n >> 1, sub + 1) : sub;
+    return  n > 1 ? collatz_rec((n & 1) ? (n * 3 + 1) / 2 : n >> 1, sub + 1 + (n & 1)) : sub;
 }
 
 int collatz(int n){
